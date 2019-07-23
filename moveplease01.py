ab8bc866720a83caca3ccf852b6212115ad4c432
Haroon Warrich <haroon.warrich@gmail.com>
2019-07-23 14:07:37 -0400

    HaroonW move file

---
+++ moveplease01.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+import shutil
+
+import os
+
+os.chdir('/home/student/mycode/')
+
+shutil.move('raynor.obj','ceph_storage/')
+
+xname = input('whatis the name for kerrigan.obj?')
+
+shutil.move('ceph_storage/kerrigan.obj', 'ceph_storage/' + xname)
+
+# the following line will rename file
+
+
+
+
